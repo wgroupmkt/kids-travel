87c43cee071cd295511b5b18318bd3be8dc4a677
Agustin Escudero <agustinescuderoweb@gmail.com>
2026-04-09 11:06:20 -0300

    primer commit

--- lib/generarImagen.ts
+++ lib/generarImagen.ts
@@ -18,12 +18,14 @@ export async function generarImagen(numero: string) {
 
   // 🔥 usar la fuente registrada
   ctx.font = "bold 30px Inter";
-  ctx.fillStyle = "#ffffff";
+  ctx.fillStyle = "#2d2a7b"; // azul del branding
+  ctx.shadowColor = "rgba(0,0,0,0.2)";
+  ctx.shadowBlur = 4;
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
 
   ctx.textAlign = "left"; // importante
-  ctx.fillText(numero, image.width * 0.78, image.height * 0.765);
+  ctx.fillText(numero, image.width * 0.63, image.height * 0.5677);
 
   return canvas.toBuffer("image/jpeg");
 }
